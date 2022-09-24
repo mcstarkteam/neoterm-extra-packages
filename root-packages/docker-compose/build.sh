ec65f1e67bc81c1d0df5880fc15c2b6f2f4d8fa2
neoterm-extra <2987456283@qq.com>
2022-09-24 15:59:41 +0800

    run on rooted android device packages

---
+++ root-packages/docker-compose/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/docker/compose
+TERMUX_PKG_DESCRIPTION="Compose is a tool for defining and running multi-container Docker applications."
+TERMUX_PKG_SHA256=e002f4f50bfb1b3c937dc0a86a8a59395182fe1288e4ed3429db5771f68f7320
+TERMUX_PKG_VERSION="2.5.0"
+TERMUX_PKG_SRCURL="https://github.com/docker/compose/archive/v${TERMUX_PKG_VERSION}.tar.gz"
+TERMUX_PKG_LICENSE="Apache-2.0"
+TERMUX_PKG_MAINTAINER="@termux"
+TERMUX_PKG_DEPENDS=docker
+
+termux_step_make() {
+	termux_setup_golang
+	export GOPATH=$TERMUX_PKG_BUILDDIR
+	cd $TERMUX_PKG_SRCDIR
+	mkdir bin/
+        if ! [ -z "$GOOS" ];then export GOOS=android;fi
+	go build -o bin/docker-compose -ldflags="-s -w -X github.com/docker/compose/v2/internal.Version=${TERMUX_PKG_VERSION}" ./cmd
+}
+
+termux_step_make_install() {
+	install -Dm755 -t "${TERMUX_PREFIX}"/bin "${TERMUX_PKG_SRCDIR}"/bin/docker-compose
+}
